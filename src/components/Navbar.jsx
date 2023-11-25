5d229c6f8c6348ff1ed16f814b97c5c24e10d35b
raulgp <raul.garrigos@hotmail.com>
2023-11-25 11:40:28 +0100

    Iteration 3 done + some styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import homeIcon from "../assets/home-icon.png";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to={"/"}>
+        <img src={homeIcon} alt="home-icon" className="home-icon" />
+      </Link>
+    </nav>
+  );
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,50 @@
 import { Link } from "react-router-dom";
+import beersPicture from "../assets/beers.png";
+import newBeer from "../assets/new-beer.png";
+import randomBeer from "../assets/random-beer.png";
 
 function HomePage() {
   return (
-    <nav>
-      <Link to={"/beers"}>All Beers</Link>
+    <div className="home-page">
+      <Link to={"/beers"}>
+        <img src={beersPicture} alt="beers-picture" className="beers-picture" />
+        <br />
+        <h1>All Beers</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut
+          risus fermentum, scelerisque enim consequat, finibus sem. Duis varius
+          condimentum rutrum.
+        </p>
+      </Link>
       <br />
-      <Link to={"/random-beer"}>Random Beer</Link>
+      <Link to={"/random-beer"}>
+        <img
+          src={randomBeer}
+          alt="random-beer-picture"
+          className="random-beer-picture"
+        />
+        <h1>Random Beer</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut
+          risus fermentum, scelerisque enim consequat, finibus sem. Duis varius
+          condimentum rutrum.
+        </p>
+      </Link>
       <br />
-      <Link to={"/new-beer"}>New Beer</Link>
-    </nav>
+      <Link to={"/new-beer"}>
+        <img
+          src={newBeer}
+          alt="new-beer-picture"
+          className="new-beer-picture"
+        />
+        <h1>New Beer</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut
+          risus fermentum, scelerisque enim consequat, finibus sem. Duis varius
+          condimentum rutrum.
+        </p>
+      </Link>
+    </div>
   );
 }
 
