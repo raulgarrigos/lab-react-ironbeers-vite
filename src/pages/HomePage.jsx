d8abfcb776777cf662ea6a3e1a875e435b64aeaf
raulgp <raul.garrigos@hotmail.com>
2023-11-25 11:13:25 +0100

    Iteration 2 done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,15 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <nav>
+      <Link to={"/beers"}>All Beers</Link>
+      <br />
+      <Link to={"/random-beer"}>Random Beer</Link>
+      <br />
+      <Link to={"/new-beer"}>New Beer</Link>
+    </nav>
+  );
+}
 
 export default HomePage;
